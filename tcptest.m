2b43d6b74f9bfddedaa7fe410c1d317c7b8ca294
mawvvii <105461506+mawvvii@users.noreply.github.com>
2022-11-20 17:50:44 +0100

    updated version 20/11/2022

--- tcptest.m
+++ tcptest.m
@@ -6,17 +6,20 @@ address = '192.168.178.56';
 port = 50907;
 
 server = tcpserver(address,port)
-sendata = ["sin", 10];
-len = length(sendata);
-x = jsonencode(sendata);
 
 waitfor(server,"Connected",1)
+out = sim('tcpsimtest.slx')
 
 disp("going into while")
-
-write(server,x);
+outv = out.simout
+input = jsonencode(outv.signals);
+write(server,input);
 disp("sending value")
 
+x = read(server,server.NumBytesAvailable,"char")
+b = jsondecode(x)
+var.signals.values = b.values
 waitfor(server,"Connected",0)
+disp("server disconnected")
 clear server
 
